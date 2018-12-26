cc360e6307a4ea521cf87c1e9b22697e93ac35e5
marouamaalej <43611150+marouamaalej@users.noreply.github.com>
2018-12-26 11:49:32 +0100

    Update add.c

--- tests/add.c
+++ tests/add.c
@@ -1,14 +1,23 @@
-
+#include<stdio.h>
+#include<stdlib.h>
 
 int add(int n, int a){
-  if (n > a)
-    n = n - a;
-  else
-    n = a - n;
-  if (n == -10)
+  if (n < a)
+    n = n + a;
+  if (n == a)
     return 0;
-  else if (n > 0)
-    return n + add(n - 1, a);
   else
-    return -n + add(n - 1, a);
+    return n + add(n - 1, a);
+}
+
+
+int main(int argc, char** argv){
+  if(argc != 3)
+    return 1;
+  int n = atoi(argv[1]);
+  int a = atoi(argv[2]);
+  int s = add(n, a);
+  printf("add %d %d = %d\n", n, a, add(n,a));
+
+return 0;
 }
